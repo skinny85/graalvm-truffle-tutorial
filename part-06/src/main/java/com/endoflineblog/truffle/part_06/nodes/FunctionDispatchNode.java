bf8160734c96aad0eb5238b30794aa771d340083
Adam Ruka <adamruka85@gmail.com>
2021-10-17 14:20:51 -0700

    Replace the indirect call node with a direct call node.

--- part-06/src/main/java/com/endoflineblog/truffle/part_06/nodes/FunctionDispatchNode.java
+++ part-06/src/main/java/com/endoflineblog/truffle/part_06/nodes/FunctionDispatchNode.java
@@ -3,15 +3,17 @@ package com.endoflineblog.truffle.part_06.nodes;
 import com.endoflineblog.truffle.part_06.runtime.FunctionObject;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.nodes.IndirectCallNode;
+import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.Node;
 
 public abstract class FunctionDispatchNode extends Node {
     public abstract Object executeDispatch(Object function, Object[] arguments);
 
     @Specialization
-    protected static Object doIndirect(FunctionObject function, Object[] arguments,
-            @Cached("create()") IndirectCallNode callNode) {
-        return callNode.call(function.callTarget, arguments);
+    protected static Object doIndirect(
+            @SuppressWarnings("unused") FunctionObject function,
+            Object[] arguments,
+            @Cached("create(function.callTarget)") DirectCallNode callNode) {
+        return callNode.call(arguments);
     }
 }
