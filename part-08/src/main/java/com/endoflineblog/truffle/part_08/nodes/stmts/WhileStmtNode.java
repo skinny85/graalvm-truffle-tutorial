934184dba537a51169b5de93b1e3b6d603e16829
Adam Ruka <adamruka85@gmail.com>
2022-06-25 12:25:44 -0700

    Refactor - make WhileRepeatingNode a private nested class of WhileStmtNode.

--- part-08/src/main/java/com/endoflineblog/truffle/part_08/nodes/stmts/WhileStmtNode.java
+++ part-08/src/main/java/com/endoflineblog/truffle/part_08/nodes/stmts/WhileStmtNode.java
@@ -5,8 +5,11 @@ import com.endoflineblog.truffle.part_08.runtime.Undefined;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.LoopNode;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RepeatingNode;
 
 public final class WhileStmtNode extends EasyScriptStmtNode {
+    @SuppressWarnings("FieldMayBeFinal")
     @Child
     private LoopNode loopNode;
 
@@ -18,5 +21,29 @@ public final class WhileStmtNode extends EasyScriptStmtNode {
     public Object executeStatement(VirtualFrame frame) {
         this.loopNode.execute(frame);
         return Undefined.INSTANCE;
+    }
+
+    private static final class WhileRepeatingNode extends Node implements RepeatingNode {
+        @SuppressWarnings("FieldMayBeFinal")
+        @Child
+        private EasyScriptExprNode conditionExpr;
+
+        @SuppressWarnings("FieldMayBeFinal")
+        @Child
+        private EasyScriptStmtNode bodyStmt;
+
+        public WhileRepeatingNode(EasyScriptExprNode conditionExpr, EasyScriptStmtNode bodyStmt) {
+            this.conditionExpr = conditionExpr;
+            this.bodyStmt = bodyStmt;
+        }
+
+        @Override
+        public boolean executeRepeating(VirtualFrame frame) {
+            if (!this.conditionExpr.executeBool(frame)) {
+                return false;
+            }
+            this.bodyStmt.executeStatement(frame);
+            return true;
+        }
     }
 }
