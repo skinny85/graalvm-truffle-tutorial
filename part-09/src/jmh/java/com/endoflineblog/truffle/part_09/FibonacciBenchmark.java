15c128c8a39adc88d86d1170ab0c22cd62a5b5b5
Adam Ruka <adamruka85@gmail.com>
2022-07-23 10:51:41 -0700

    Introduce an abstract superclass of the benchmark.

---
+++ part-09/src/jmh/java/com/endoflineblog/truffle/part_09/FibonacciBenchmark.java
@@ -0,0 +1,37 @@
+package com.endoflineblog.truffle.part_09;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+
+public class FibonacciBenchmark extends TruffleBenchmark {
+    private static final String FIBONACCI_JS_PROGRAM = "" +
+            "function fib(n) { " +
+            "    if (n > -2) { " +
+            "        return Math.abs(n); " +
+            "    } " +
+            "    return fib(n + 1) + fib(n + 2); " +
+            "} " +
+            "fib(-20);";
+
+    @Benchmark
+    public int recursive_ezs_eval() {
+        return this.truffleContext.eval("ezs", FIBONACCI_JS_PROGRAM).asInt();
+    }
+
+    @Fork(jvmArgsAppend = "-Dgraalvm.locatorDisabled=false")
+    @Benchmark
+    public int recursive_js_eval() {
+        return this.truffleContext.eval("js", FIBONACCI_JS_PROGRAM).asInt();
+    }
+
+    @Benchmark
+    public int recursive_java() {
+        return fibonacciRecursive(-20);
+    }
+
+    public static int fibonacciRecursive(int n) {
+        return n > -2
+                ? Math.abs(n)
+                : fibonacciRecursive(n + 1) + fibonacciRecursive(n + 2);
+    }
+}
--- part-09/src/jmh/java/com/endoflineblog/truffle/part_09/TruffleBenchmark.java
+++ part-09/src/jmh/java/com/endoflineblog/truffle/part_09/TruffleBenchmark.java
@@ -1,7 +1,6 @@
 package com.endoflineblog.truffle.part_09;
 
 import org.graalvm.polyglot.Context;
-import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
@@ -21,17 +20,8 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @State(Scope.Benchmark)
-public class TruffleBenchmark {
-    private static final String FIBONACCI_JS_PROGRAM = "" +
-            "function fib(n) { " +
-            "    if (n > -2) { " +
-            "        return Math.abs(n); " +
-            "    } " +
-            "    return fib(n + 1) + fib(n + 2); " +
-            "} " +
-            "fib(-20);";
-
-    private Context truffleContext;
+public abstract class TruffleBenchmark {
+    protected Context truffleContext;
 
     @Setup
     public void setup() {
@@ -41,31 +31,5 @@ public class TruffleBenchmark {
     @TearDown
     public void tearDown() {
         this.truffleContext.close();
-    }
-
-    @Benchmark
-    public int fibonacci_recursive_ezs_slow() {
-        return this.truffleContext.eval("ezs", FIBONACCI_JS_PROGRAM).asInt();
-    }
-
-    @Fork(jvmArgsAppend = {
-            "-Dgraalvm.locatorDisabled=false",
-            "-Dgraal.Dump=:1",
-            "-Dgraal.PrintGraph=Network"
-    })
-    @Benchmark
-    public int fibonacci_recursive_js_slow() {
-        return this.truffleContext.eval("js", FIBONACCI_JS_PROGRAM).asInt();
-    }
-
-    @Benchmark
-    public int fibonacci_recursive_java() {
-        return fibonacciRecursive(-20);
-    }
-
-    public static int fibonacciRecursive(int n) {
-        return n > -2
-                ? Math.abs(n)
-                : fibonacciRecursive(n + 1) + fibonacciRecursive(n + 2);
     }
 }
