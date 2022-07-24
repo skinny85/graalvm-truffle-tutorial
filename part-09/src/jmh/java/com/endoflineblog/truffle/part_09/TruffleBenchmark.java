61da07ca46a1caa2b34175db1eaf0fabb2b54e8e
Adam Ruka <adamruka85@gmail.com>
2022-07-24 12:33:38 -0700

    Change warmup to 10 iterations instead of 20.

--- part-09/src/jmh/java/com/endoflineblog/truffle/part_09/TruffleBenchmark.java
+++ part-09/src/jmh/java/com/endoflineblog/truffle/part_09/TruffleBenchmark.java
@@ -14,7 +14,7 @@ import org.openjdk.jmh.annotations.Warmup;
 
 import java.util.concurrent.TimeUnit;
 
-@Warmup(iterations = 20, time = 1)
+@Warmup(iterations = 10, time = 1)
 @Measurement(iterations = 5, time = 1)
 @Fork(value = 1, jvmArgsAppend = "-Dgraalvm.locatorDisabled=true")
 @BenchmarkMode(Mode.AverageTime)
