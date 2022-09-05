dd7f8878557d3c4c1c16dcae71a81e418f10dd43
Adam Ruka <adamruka85@gmail.com>
2022-09-04 17:28:02 -0700

    Switch to using a Fibonacci version with subtraction in the benchmarks.

--- part-09/src/jmh/java/com/endoflineblog/truffle/part_09/FibonacciBenchmark.java
+++ part-09/src/jmh/java/com/endoflineblog/truffle/part_09/FibonacciBenchmark.java
@@ -4,13 +4,14 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 
 public class FibonacciBenchmark extends TruffleBenchmark {
-    private static final String FIBONACCI_JS_PROGRAM = "" +
+    private static final String FIBONACCI_JS_FUNCTION = "" +
             "function fib(n) { " +
-            "    if (n > -2) " +
+            "    if (n < 2) { " +
             "        return 1; " +
-            "    return fib(n + 1) + fib(n + 2); " +
-            "} " +
-            "fib(-20);";
+            "    } " +
+            "    return fib(n - 1) + fib(n - 2); " +
+            "} ";
+    private static final String FIBONACCI_JS_PROGRAM = FIBONACCI_JS_FUNCTION + "fib(20);";
 
     @Benchmark
     public int recursive_ezs_eval() {
@@ -25,13 +26,7 @@ public class FibonacciBenchmark extends TruffleBenchmark {
 
     @Benchmark
     public int recursive_sl_eval() {
-        return this.truffleContext.eval("sl", "" +
-                "function fib(n) { " +
-                "    if (n < 2) { " +
-                "        return 1; " +
-                "    } " +
-                "    return fib(n - 1) + fib(n - 2); " +
-                "} " +
+        return this.truffleContext.eval("sl", FIBONACCI_JS_FUNCTION +
                 "function main() { " +
                 "    return fib(20); " +
                 "} ").asInt();
@@ -39,12 +34,12 @@ public class FibonacciBenchmark extends TruffleBenchmark {
 
     @Benchmark
     public int recursive_java() {
-        return fibonacciRecursive(-20);
+        return fibonacciRecursive(20);
     }
 
     public static int fibonacciRecursive(int n) {
-        return n > -2
+        return n < 2
                 ? 1
-                : fibonacciRecursive(n + 1) + fibonacciRecursive(n + 2);
+                : fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
     }
 }
