bf7938fe47ee234810168d5fb0aa4e9db02f4f3e
Adam Ruka <adamruka85@gmail.com>
2022-11-29 22:54:55 -0800

    Add EasyScript and JS bubble sort benchmarks, and temporarily remove the Fibonacci benchmark.

--- part-10/src/jmh/java/com/endoflineblog/truffle/part_10/BubbleSortBenchmark.java
+++ part-10/src/jmh/java/com/endoflineblog/truffle/part_10/BubbleSortBenchmark.java
@@ -1,10 +1,15 @@
 package com.endoflineblog.truffle.part_10;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class BubbleSortBenchmark extends TruffleBenchmark {
     private static final int arraySize = 1_000;
     private static final int[] descendinglySortedArray;
+    private static final String JS_BUBBLE_SORT;
 
     static {
         descendinglySortedArray = new int[arraySize];
@@ -12,6 +17,23 @@ public class BubbleSortBenchmark extends TruffleBenchmark {
             descendinglySortedArray[i] = arraySize - i;
 //            descendinglySortedArray[i] = i;
         }
+        JS_BUBBLE_SORT = "" +
+                "function bubbleSort() { " +
+                "    const array = " + Arrays.stream(descendinglySortedArray)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(", ", "[", "]")) + "; " +
+                "    for (var i = 0; i < " + arraySize + " - 1; i = i + 1) { " +
+                "        for (var j = 0; j < " + arraySize + " - 1 - i; j = j + 1) { " +
+                "            if (array[j] > array[j + 1]) { " +
+                "                var tmp = array[j]; " +
+                "                array[j] = array[j + 1]; " +
+                "                array[j + 1] = tmp; " +
+                "            } " +
+                "        } " +
+                "    } " +
+                "    return array; " +
+                "} " +
+                "bubbleSort(); ";
     }
 
     @Benchmark
@@ -21,12 +43,23 @@ public class BubbleSortBenchmark extends TruffleBenchmark {
         return array;
     }
 
+    @Benchmark
+    public long ezs() {
+        return this.truffleContext.eval("ezs", JS_BUBBLE_SORT).getArraySize();
+    }
+
+    @Fork(jvmArgsAppend = "-Dgraalvm.locatorDisabled=false")
+    @Benchmark
+    public long js() {
+        return this.truffleContext.eval("js", JS_BUBBLE_SORT).getArraySize();
+    }
+
     static void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
+        for (var i = 0; i < array.length - 1; i = i + 1) {
+            for (var j = 0; j < array.length - 1 - i; j = j + 1) {
                 if (array[j] > array[j + 1]) {
                     // swap j and j+1
-                    int tmp = array[j];
+                    var tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
                 }
