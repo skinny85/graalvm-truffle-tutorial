b535ec3d348616083a0fe7bc02928470a85b59f5
Adam Ruka <adamruka85@gmail.com>
2023-03-14 22:24:36 -0700

    Write a benchmark using indexing instead of direct property access.

--- part-11/src/jmh/java/com/endoflineblog/truffle/part_11/StringLengthBenchmark.java
+++ part-11/src/jmh/java/com/endoflineblog/truffle/part_11/StringLengthBenchmark.java
@@ -3,56 +3,21 @@ package com.endoflineblog.truffle.part_11;
 import org.openjdk.jmh.annotations.Benchmark;
 
 public class StringLengthBenchmark extends TruffleBenchmark {
-    private static final int INPUT = 1_400_000;
+    private static final int INPUT = 1_000_000;
 
     @Override
     public void setup() {
         super.setup();
 
-        this.truffleContext.eval("ezs", COUNT_IN_WHILE_LOOP_FUNC_NO_ARG);
-        this.truffleContext.eval("js", COUNT_IN_WHILE_LOOP_FUNC_NO_ARG);
+        this.truffleContext.eval("ezs", COUNT_WHILE_SUBSTR_LEN_PROP);
+        this.truffleContext.eval("js", COUNT_WHILE_SUBSTR_LEN_PROP);
 
-        this.truffleContext.eval("ezs", COUNT_IN_WHILE_LOOP_FUNC_WITH_ARG);
-        this.truffleContext.eval("js", COUNT_IN_WHILE_LOOP_FUNC_WITH_ARG);
+        this.truffleContext.eval("ezs", COUNT_WHILE_SUBSTR_LEN_INDEX);
+        this.truffleContext.eval("js", COUNT_WHILE_SUBSTR_LEN_INDEX);
     }
 
-    public static final String COUNT_IN_WHILE_LOOP_FUNC_NO_ARG = "" +
-            "function countInWhileLoopNoArg() { " +
-            "    var ret = 0, n = " + INPUT + "; " +
-            "    while (n > 0) { " +
-            "        n = n - 'ALU'.substring(0, 1).length; " +
-            "        ret = ret + 1; " +
-            "    } " +
-            "    return ret; " +
-            "}";
-
-    static int countInWhileLoopNoArg() {
-        int ret = 0, n = INPUT;
-        while (n > 0) {
-//            n = n - 1;
-            n = n - "ALU".substring(0, 1).length();
-            ret = ret + 1;
-        }
-        return ret;
-    }
-
-    @Benchmark
-    public int count_in_while_loop_func_no_arg_ezs() {
-        return this.truffleContext.eval("ezs", "countInWhileLoopNoArg();").asInt();
-    }
-
-    @Benchmark
-    public int count_in_while_loop_func_no_arg_js() {
-        return this.truffleContext.eval("js", "countInWhileLoopNoArg();").asInt();
-    }
-
-    @Benchmark
-    public int count_in_while_loop_func_no_arg_java() {
-        return countInWhileLoopNoArg();
-    }
-
-    private static final String COUNT_IN_WHILE_LOOP_FUNC_WITH_ARG = "" +
-            "function countInWhileLoopWithArg(n) { " +
+    private static final String COUNT_WHILE_SUBSTR_LEN_PROP = "" +
+            "function countWhileSubstrLenProp(n) { " +
             "    var ret = 0; " +
             "    while (n > 0) { " +
             "        n = n - 'ALU'.substring(0, 1).length; " +
@@ -61,10 +26,9 @@ public class StringLengthBenchmark extends TruffleBenchmark {
             "    return ret; " +
             "}";
 
-    static int countInWhileLoopWithArg(int n) {
+    static int countWhileJava(int n) {
         int ret = 0;
         while (n > 0) {
-//            n = n - 1;
             n = n - "ALU".substring(0, 1).length();
             ret = ret + 1;
         }
@@ -72,17 +36,37 @@ public class StringLengthBenchmark extends TruffleBenchmark {
     }
 
     @Benchmark
-    public int count_in_while_loop_func_with_arg_ezs() {
-        return this.truffleContext.eval("ezs", "countInWhileLoopWithArg(" + INPUT + ");").asInt();
+    public int count_while_substr_len_prop_ezs() {
+        return this.truffleContext.eval("ezs", "countWhileSubstrLenProp(" + INPUT + ");").asInt();
     }
 
     @Benchmark
-    public int count_in_while_loop_func_with_arg_js() {
-        return this.truffleContext.eval("js", "countInWhileLoopWithArg(" + INPUT + ");").asInt();
+    public int count_while_substr_len_prop_js() {
+        return this.truffleContext.eval("js", "countWhileSubstrLenProp(" + INPUT + ");").asInt();
     }
 
     @Benchmark
-    public int count_in_while_loop_func_with_arg_java() {
-        return countInWhileLoopWithArg(INPUT);
+    public int count_while_substr_len_prop_java() {
+        return countWhileJava(INPUT);
+    }
+
+    private static final String COUNT_WHILE_SUBSTR_LEN_INDEX = "" +
+            "function countWhileSubstrLenIndex(n) { " +
+            "    var ret = 0; " +
+            "    while (n > 0) { " +
+            "        n = n - 'ALU'['substring'](0, 1)['length']; " +
+            "        ret = ret + 1; " +
+            "    } " +
+            "    return ret; " +
+            "}";
+
+    @Benchmark
+    public int count_while_substr_len_index_ezs() {
+        return this.truffleContext.eval("ezs", "countWhileSubstrLenIndex(" + INPUT + ");").asInt();
+    }
+
+    @Benchmark
+    public int count_while_substr_len_index_js() {
+        return this.truffleContext.eval("js", "countWhileSubstrLenIndex(" + INPUT + ");").asInt();
     }
 }
