13f31f8cb5d167eb86b901e69984e6e16d4112ad
Adam Ruka <adamruka85@gmail.com>
2023-03-25 15:37:32 -0700

    Implement property access with `TruffleString` instead of Java strings.

--- part-11/src/main/java/com/endoflineblog/truffle/part_11/nodes/exprs/strings/ReadTruffleStringPropertyExprNode.java
+++ part-11/src/main/java/com/endoflineblog/truffle/part_11/nodes/exprs/strings/ReadTruffleStringPropertyExprNode.java
@@ -12,7 +12,9 @@ import com.oracle.truffle.api.strings.TruffleString;
 @ImportStatic(EasyScriptTruffleStrings.class)
 public abstract class ReadTruffleStringPropertyExprNode extends EasyScriptNode {
     protected static final String LENGTH_PROP = "length";
+    protected static final TruffleString LENGTH_PROP_TS = EasyScriptTruffleStrings.fromJavaString(LENGTH_PROP);
     protected static final String CHAR_AT_PROP = "charAt";
+    protected static final TruffleString CHAR_AT_PROP_TS = EasyScriptTruffleStrings.fromJavaString(CHAR_AT_PROP);
 
     public abstract Object executeReadTruffleStringProperty(TruffleString truffleString, Object property);
 
@@ -27,21 +29,23 @@ public abstract class ReadTruffleStringPropertyExprNode extends EasyScriptNode {
                 : EasyScriptTruffleStrings.substring(truffleString, index, 1, substringNode);
     }
 
-    @Specialization(guards = "LENGTH_PROP.equals(propertyName)")
+    @Specialization(guards = "equals(LENGTH_PROP_TS, propertyName, equalNode)")
     protected int readLengthProperty(
             TruffleString truffleString,
-            @SuppressWarnings("unused") String propertyName,
+            @SuppressWarnings("unused") TruffleString propertyName,
+            @Cached TruffleString.EqualNode equalNode,
             @Cached TruffleString.CodePointLengthNode lengthNode) {
         return EasyScriptTruffleStrings.length(truffleString, lengthNode);
     }
 
     @Specialization(guards = {
-            "CHAR_AT_PROP.equals(propertyName)",
+            "equals(CHAR_AT_PROP_TS, propertyName, equalNode)",
             "same(charAtMethod.methodTarget, truffleString)"
     })
     protected FunctionObject readCharAtProperty(
             @SuppressWarnings("unused") TruffleString truffleString,
-            @SuppressWarnings("unused") String propertyName,
+            @SuppressWarnings("unused") TruffleString propertyName,
+            @Cached TruffleString.EqualNode equalNode,
             @Cached("create(currentLanguageContext().stringPrototype.charAtMethod, 2, truffleString)") FunctionObject charAtMethod) {
         return charAtMethod;
     }
