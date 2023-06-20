02ca5e9b05bc741a7dfb5f24b7ab887a6e19c828
Adam Ruka <adamruka85@gmail.com>
2023-06-19 18:28:10 -0700

    Improve the benchmark by caching the DynamicObjectLibrary in ObjectLiteralKeyValueNode.

--- part-12/src/main/java/com/endoflineblog/truffle/part_12/nodes/exprs/objects/ObjectLiteralKeyValueNode.java
+++ part-12/src/main/java/com/endoflineblog/truffle/part_12/nodes/exprs/objects/ObjectLiteralKeyValueNode.java
@@ -3,6 +3,7 @@ package com.endoflineblog.truffle.part_12.nodes.exprs.objects;
 import com.endoflineblog.truffle.part_12.nodes.EasyScriptNode;
 import com.endoflineblog.truffle.part_12.nodes.exprs.EasyScriptExprNode;
 import com.endoflineblog.truffle.part_12.runtime.JavaScriptObject;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.object.DynamicObjectLibrary;
 
@@ -13,13 +14,21 @@ public final class ObjectLiteralKeyValueNode extends EasyScriptNode {
     @SuppressWarnings("FieldMayBeFinal")
     private EasyScriptExprNode value;
 
+    @Child
+    private DynamicObjectLibrary dynamicObjectLibrary;
+
     public ObjectLiteralKeyValueNode(String key, EasyScriptExprNode value) {
         this.key = key;
         this.value = value;
     }
 
     public void executeObjectWriteGeneric(VirtualFrame frame, JavaScriptObject object) {
+        if (this.dynamicObjectLibrary == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            this.dynamicObjectLibrary = this.insert(DynamicObjectLibrary.getFactory().createDispatched(1));
+        }
+
         Object value = this.value.executeGeneric(frame);
-        DynamicObjectLibrary.getUncached().putWithFlags(object, this.key, value, 0);
+        this.dynamicObjectLibrary.putWithFlags(object, this.key, value, 0);
     }
 }
