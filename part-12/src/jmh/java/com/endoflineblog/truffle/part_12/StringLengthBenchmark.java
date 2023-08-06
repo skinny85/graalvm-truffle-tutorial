faa53d1c7668db80f446fc2fc6509c66b338f99a
Adam Ruka <adamruka85@gmail.com>
2023-08-06 13:37:43 -0700

    Bring back the StringLengthBenchmark to make ReadTruffleStringPropertyExprNode fast again.

---
+++ part-12/src/jmh/java/com/endoflineblog/truffle/part_12/StringLengthBenchmark.java
@@ -0,0 +1,58 @@
+package com.endoflineblog.truffle.part_12;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class StringLengthBenchmark extends TruffleBenchmark {
+    private static final int INPUT = 1_000_000;
+
+    @Override
+    public void setup() {
+        super.setup();
+
+        this.truffleContext.eval("ezs", COUNT_WHILE_CHAR_AT_DIRECT_PROP);
+        this.truffleContext.eval("js", COUNT_WHILE_CHAR_AT_DIRECT_PROP);
+
+        this.truffleContext.eval("ezs", COUNT_WHILE_CHAR_AT_INDEX_PROP);
+        this.truffleContext.eval("js", COUNT_WHILE_CHAR_AT_INDEX_PROP);
+    }
+
+    private static final String COUNT_WHILE_CHAR_AT_DIRECT_PROP = "" +
+            "function countWhileCharAtDirectProp(n) { " +
+            "    var ret = 0; " +
+            "    while (n > 0) { " +
+            "        n = n - ('a'.charAt(0) + ''.charAt()).length; " +
+            "        ret = ret + 1; " +
+            "    } " +
+            "    return ret; " +
+            "}";
+
+    @Benchmark
+    public int count_while_char_at_direct_prop_ezs() {
+        return this.truffleContext.eval("ezs", "countWhileCharAtDirectProp(" + INPUT + ");").asInt();
+    }
+
+    @Benchmark
+    public int count_while_char_at_direct_prop_js() {
+        return this.truffleContext.eval("js", "countWhileCharAtDirectProp(" + INPUT + ");").asInt();
+    }
+
+    private static final String COUNT_WHILE_CHAR_AT_INDEX_PROP = "" +
+            "function countWhileCharAtIndexProp(n) { " +
+            "    var ret = 0; " +
+            "    while (n > 0) { " +
+            "        n = n - ('a'['charAt'](0) + ''['charAt']())['length']; " +
+            "        ret = ret + 1; " +
+            "    } " +
+            "    return ret; " +
+            "}";
+
+    @Benchmark
+    public int count_while_char_at_index_prop_ezs() {
+        return this.truffleContext.eval("ezs", "countWhileCharAtIndexProp(" + INPUT + ");").asInt();
+    }
+
+    @Benchmark
+    public int count_while_char_at_index_prop_js() {
+        return this.truffleContext.eval("js", "countWhileCharAtIndexProp(" + INPUT + ");").asInt();
+    }
+}
