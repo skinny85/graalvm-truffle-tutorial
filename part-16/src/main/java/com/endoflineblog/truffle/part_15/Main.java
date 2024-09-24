ad683cc622c127a6a5cd73931802a539ca09a22e
Adam Ruka <adamruka85@gmail.com>
2024-09-23 22:46:42 -0700

    Add simple Main program that executes a hard-coded JS file

---
+++ part-16/src/main/java/com/endoflineblog/truffle/part_15/Main.java
@@ -0,0 +1,23 @@
+package com.endoflineblog.truffle.part_15;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Source source = Source
+                .newBuilder("ezs", new File("src/main/resources/example.js"))
+                .build();
+
+        try (Context context = Context
+                .newBuilder()
+                .build()) {
+            Value result = context.eval(source);
+            System.out.println(result.toString());
+        }
+    }
+}
