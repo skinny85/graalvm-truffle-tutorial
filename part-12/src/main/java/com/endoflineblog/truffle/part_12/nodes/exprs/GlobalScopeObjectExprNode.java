32bdf11285489ea633745a197f7a35e23fc6bb1d
Adam Ruka <adamruka85@gmail.com>
2026-09-16 21:33:31 -0700

    Annotate @GenerateInline(false) with GlobalScopeObjectExprNode

--- part-12/src/main/java/com/endoflineblog/truffle/part_12/nodes/exprs/GlobalScopeObjectExprNode.java
+++ part-12/src/main/java/com/endoflineblog/truffle/part_12/nodes/exprs/GlobalScopeObjectExprNode.java
@@ -1,5 +1,6 @@
 package com.endoflineblog.truffle.part_12.nodes.exprs;
 
+import com.oracle.truffle.api.dsl.GenerateInline;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.object.DynamicObject;
 
@@ -15,6 +16,7 @@ import com.oracle.truffle.api.object.DynamicObject;
  * annotation in their {@link Specialization} methods.
  * Identical to the class with the same name from part 11.
  */
+@GenerateInline(false)
 public abstract class GlobalScopeObjectExprNode extends EasyScriptExprNode {
     @Specialization
     protected DynamicObject returnGlobalScopeObject() {
